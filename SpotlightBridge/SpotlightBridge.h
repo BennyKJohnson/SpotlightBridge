594cf9cd0cb15dbb08966a02d46fa011b5c8ad9b
Benjamin Johnson <bkj986@uowmail.edu.au>
2020-07-26 19:49:18 +1000

    Initial Commit

---
+++ SpotlightBridge/SpotlightBridge.h
@@ -0,0 +1,15 @@
+//
+//  SpotlightBridge.h
+//  SpotlightBridge
+//
+//  Created by Benjamin Johnson on 26/7/20.
+//Copyright © 2020 Benjamin Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpotlightBridge : NSObject
+
++ (instancetype)sharedInstance;
+
+@end
